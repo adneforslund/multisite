97498cde56f34fa5c53a1e27b81523f5265f5e4f
Ådne Forslund <adne.forslund@gmail.com>
2021-11-19 13:51:16 +0100

    swicth site

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,53 @@
+const sites = {
+  korea: {
+    locales: {
+      en: {
+        title: "Korean English",
+        description: "This is the description for the first site.",
+      },
+      no: {
+        title: "Korean Norwegian",
+        description: "Første side.",
+      },
+    },
+    domain: "koreatest.vercel.app",
+  },
+  angola: {
+    locales: {
+      en: {
+        title: "Angola english",
+        description: "This is the description for the second site.",
+      },
+      no: {
+        title: "Angola Norwegian",
+        description: "Second site norwegian.",
+      },
+    },
+    domain: "angolatest.vercel.app",
+  },
+};
+const siteKeys = Object.keys(sites);
+
+const pseudoLocales = ["en", "no"];
+
 module.exports = {
-  reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    sites,
+    pseudoLocales,
+  },
+  i18n: {
+    locales: siteKeys,
+    defaultLocale: siteKeys[0],
+    domains: siteKeys.map((siteKey) => ({
+      domain: sites[siteKey].domain,
+      defaultLocale: siteKey,
+    })),
+  },
+  redirects: () => [
+    {
+      source: "/",
+      destination: `/${pseudoLocales[0]}`,
+      permanent: true,
+    },
+  ],
+};
---
+++ web/pages/[pseudoLocale]/index.js
@@ -0,0 +1,98 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import getConfig from "next/config";
+
+export default function Home({ pseudoLocale }) {
+  const { locales, locale, defaultLocale } = useRouter();
+  const {
+    publicRuntimeConfig: { sites, pseudoLocales },
+  } = getConfig();
+
+  const nextLocale = pseudoLocales.find(
+    (otherLocale) => otherLocale !== pseudoLocale
+  );
+  const nextSiteNr = locale === "korea" ? 1 : 2;
+
+  const siteAndLocale = sites[locale] && sites[locale].locales[pseudoLocale];
+
+  return (
+    <div>
+      <Head>
+        <title>Multisite</title>
+        <meta name="description" content="hei" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          {siteAndLocale
+            ? siteAndLocale.title
+            : `Site not found: '${locale}</strong>`}
+        </h1>
+        {siteAndLocale && <p>{siteAndLocale.description}</p>}
+
+        <div>
+          <div>
+            <h3>
+              Switch site (<code>locale</code>)
+            </h3>
+            <p>
+              Currently: <strong>{locale}</strong>
+            </p>
+            <p>Go to: site/domain {nextSiteNr}</p>
+          </div>
+
+          <Link href={`/${nextLocale}`}>
+            <a>
+              <h3>
+                Switch language (<code>pseudoLocale</code>)
+              </h3>
+              <p>
+                Currently: <strong>{pseudoLocale}</strong>
+              </p>
+              <p>
+                Switch locale to: <strong>{nextLocale}</strong>
+              </p>
+            </a>
+          </Link>
+
+          <div>
+            <h3>Props</h3>
+            <p>
+              locale (useRouter):{" "}
+              <code>
+                {JSON.stringify({ locales, locale, defaultLocale }, null, 2)}
+              </code>
+            </p>
+            <p>
+              pseudoLocale:{" "}
+              <code>
+                {JSON.stringify({ pseudoLocales, pseudoLocale }, null, 2)}
+              </code>
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export async function getStaticProps({
+  params: { pseudoLocale = "en" },
+  locale = "korea",
+}) {
+  return {
+    props: {
+      pseudoLocale,
+    },
+    revalidate: 60,
+  };
+}
+
+export async function getStaticPaths({ locales }) {
+  return {
+    paths: [{ params: { pseudoLocale: "en" }, locale: "korea" }],
+    fallback: true,
+  };
+}
